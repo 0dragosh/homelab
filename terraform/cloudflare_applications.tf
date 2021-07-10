eea0f79de3bdb609295ed6e40d84799720861e4f
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-07-10 20:02:09 +0300

    Add terraform

--- terraform/cloudflare_applications.tf
+++ terraform/cloudflare_applications.tf
@@ -6,11 +6,11 @@ locals {
 }
 
 resource "cloudflare_access_application" "app" {
-  for_each = local.apps
+  for_each = {for i, app in local.apps:  i => app}
 
   zone_id                   = var.zone_id
   name                      = each.value
-  domain                    = ".${var.domain}"
+  domain                    = "${each.value}.${var.domain}"
   type                      = "self_hosted"
   session_duration          = "24h"
   auto_redirect_to_identity = true
