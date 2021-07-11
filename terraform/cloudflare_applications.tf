9a02be8ea73746225b4fc9ed914f5a434b90a71e
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-07-11 14:13:24 +0300

    tf changes

--- terraform/cloudflare_applications.tf
+++ terraform/cloudflare_applications.tf
@@ -14,7 +14,7 @@ locals {
 }
 
 resource "cloudflare_access_application" "app" {
-  for_each = {for i, app in local.apps:  i => app}
+  for_each = {for app in local.apps:  app => app}
 
   zone_id                   = var.zone_id
   name                      = each.value
@@ -25,7 +25,7 @@ resource "cloudflare_access_application" "app" {
 }
 
 resource "cloudflare_access_policy" "policy" {
-  for_each = {for i, app in cloudflare_access_application.app:  i => app}
+  for_each = cloudflare_access_application.app
 
   zone_id        = var.zone_id
   application_id = each.value.id
