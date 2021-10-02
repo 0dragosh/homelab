736883a16e15838b2649fa7a0741bf8bb7bac4d2
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-10-02 15:40:03 +0300

    Add wg-server

--- terraform/cloudflare_applications.tf
+++ terraform/cloudflare_applications.tf
@@ -17,6 +17,7 @@ locals {
     "sonarr",
     "storage",
     "tautulli",
+    "vpn"
   ]
 
   apps_token = [
@@ -27,7 +28,7 @@ locals {
 }
 
 resource "cloudflare_access_application" "app" {
-  for_each = {for app in local.apps:  app => app}
+  for_each = { for app in local.apps : app => app }
 
   zone_id                   = var.zone_id
   name                      = each.value
@@ -43,12 +44,12 @@ resource "cloudflare_access_policy" "policy" {
   zone_id        = var.zone_id
   application_id = each.value.id
 
-  name           = "allow ${var.email_domain}"
-  precedence     = "10"
-  decision       = "allow"
+  name       = "allow ${var.email_domain}"
+  precedence = "10"
+  decision   = "allow"
 
   include {
-    service_token = contains(local.apps_token,each.value.name) ? [cloudflare_access_service_token.token.id] : []
+    service_token = contains(local.apps_token, each.value.name) ? [cloudflare_access_service_token.token.id] : []
     email_domain  = [var.email_domain]
   }
 }
@@ -57,17 +58,17 @@ resource "cloudflare_access_policy" "requests" {
   zone_id        = var.zone_id
   application_id = cloudflare_access_application.app["plex-requests"].id
 
-  name           = "allow github & gsuite for requests"
-  precedence     = "20"
-  decision       = "allow"
+  name       = "allow github & gsuite for requests"
+  precedence = "20"
+  decision   = "allow"
 
   include {
-#    login_method = [cloudflare_access_identity_provider.github_oauth.id, cloudflare_access_identity_provider.gsuite.id]
+    #    login_method = [cloudflare_access_identity_provider.github_oauth.id, cloudflare_access_identity_provider.gsuite.id]
     login_method = [cloudflare_access_identity_provider.gsuite.id]
   }
 }
 
 resource "cloudflare_access_service_token" "token" {
-  zone_id        = var.zone_id
-  name           = "service token"
+  zone_id = var.zone_id
+  name    = "service token"
 }
